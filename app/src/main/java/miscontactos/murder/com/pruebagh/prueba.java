7e223a214fee898c3ff49e4704f7eef05cd5b66c
nestor lopez <nestor.lopez.desarrollador@gmail.com>
2015-05-12 15:10:39 -0500

    prueba de segundo commit

---
+++ app/src/main/java/miscontactos/murder/com/pruebagh/prueba.java
@@ -0,0 +1,12 @@
+package miscontactos.murder.com.pruebagh;
+
+/**
+ * Created by nestor on 12/05/2015.
+ */
+public class prueba {
+
+    public String Nombre= "nestor";
+    public String apellido= "lopez";
+    public String nick= "Murder";
+
+}
